5701dc4d2c9bd635ac8709c6f1aa28eb05ba5182
Acheampongjoojo <acheampongjoojo29@gmail.com>
2026-06-20 23:08:13 +0000

    Update

--- src/containers/whatGPT5/WhatGPT5.jsx
+++ src/containers/whatGPT5/WhatGPT5.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './WhatGPT5.css'
+import Features from '../features/Features'
+
 const WhatGPT5 = () => {
   return (
-    <div>
-      WhatGPT5
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3' >
+      <div className="gpt3__whatgpt3-feature">
+        <Features />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Features />
+        <Features />
+        <Features />
+      </div>
     </div>
   )
 }
